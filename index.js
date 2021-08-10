3c1718625ba045d8f0fbe21dadba4236364c32b8
wahudamon <afwa.bagas.1999@gmail.com>
2021-08-10 11:01:53 +0700

    feat: create function for check palindrome that simpler than before

--- index.js
+++ index.js
@@ -8,6 +8,20 @@ function checkPalindrome(param) {
   return reversedWord == param ? true : false;
 }
 
-checkPalindrome('madam'); // will return 'true'
-checkPalindrome('b'); // will return 'true'
-checkPalindrome('brown'); // will return 'false'
+function simplifiedCheckPalindrome(param) {
+  for(let i = 0; i < param.length; i++) {
+    if(param.charAt(i) != param.charAt((param.length - 1) - i)) {
+      return false;
+    }
+
+    return true;
+  }
+}
+
+//checkPalindrome('madam'); // will return 'true'
+//checkPalindrome('b'); // will return 'true'
+//checkPalindrome('brown'); // will return 'false'
+
+simplifiedCheckPalindrome('madam');
+simplifiedCheckPalindrome('b');
+simplifiedCheckPalindrome('brown');
